f9d7eacc0101e86c7bf9f347281ed990e7a6def2
Ronando Long <ronando.long@gmail.com>
2015-12-02 13:46:15 -0800

    Initial commit. It will work as a to do list, accepting input, crossing off line item input and deleting input with a slideUp animation.

---
+++ assets/js/scripts.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+// 7b. recall localStorage on refresh, place on page
+    $("#list-items").html(localStorage.getItem("listItems"));
+
+// ADD INPUT TO PAGE
+  // 1a. grab form's submit event handler
+    $(".add-items").submit(function(event) {
+  // 1. grab input: confirm w/console.log, use variable to grab input value
+        event.preventDefault();
+        var item = $("#todo-list-item").val();
+  // 4. stop blank <li> entries using (IF/else)
+        if (item) {  // if there is text inputted, then put input on page, (implicit: else don't)
+// INPUT
+  // 2. put input up on page w/checkbox, x & <hr>
+            $("#list-items").append("<li><input class='checkbox' type='checkbox'>" + item + "<a class='remove'>x</a><hr></li>");
+  // 7a. save to localStorage: .setItem("key", $("value")
+            localStorage.setItem("listItems", $("#list-items").html());
+  // 3. clear value out of input field
+            $("#todo-list-item").val("");
+        }
+    });
+
+// CHECKBOX TWEAKS
+  // 5. make chackbox strikethrough text: .on("event", "selector", function() {
+    $(document).on("change", ".checkbox", function() {  // Remember, we're working with dynamic element here!
+  // 8. keep checkbox checked/unchecked on refresh: if/else, "checked" is an attribute
+        if ($(this).attr("checked")) {  // IF attribute "checked" is present??
+            $(this).removeAttr("checked");  // then remove the attribute from input
+        } else {
+          $(this).attr("checked", "checked");  // otherwise check the checked attrib.
+        }
+        $(this).parent().toggleClass("completed"); // each time clicked, it swaps completed
+  // 7d. save checkbox action to localStorage
+        localStorage.setItem("listItems", $("#list-items").html());
+    });
+
+// DELETE LINE ITEM TWEAKS
+  // 6. make x remove <li> item: .on("event", "selector", function() {
+    $(document).on("click", ".remove", function() {
+        $(this).parent().slideUp( 900, function() {  // animation slideUp really sloww
+            $(this).remove();
+  // 7c. save to localStorage: .setItem("key", $("value")
+            localStorage.setItem("listItems", $("#list-items").html());
+        });
+
+    });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+//
